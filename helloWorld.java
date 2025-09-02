fd8c1dfb93990714b60129e86462b8dd86869a08
mariofgon <0769210@my.scccd.edu>
2025-09-02 11:53:33 -0700

    Add files via upload

---
+++ helloWorld.java
@@ -0,0 +1,21 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+
+public class helloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("I am new to coding");
+        System.out.println("I am returning to college");
+        System.out.println("My major is Automotive Technology");
+        System.out.println("Sorry I completed this late");
+        System.out.println("I was out of town the first week");
+        System.out.println("I learned the system shortcut with sout");
+        System.out.println("So far I think I'm understanding");
+        System.out.println("Thanks to my partner Derren");
+        System.out.println("And tons of youtube Videos");
+        System.out.println("Thanks for playing");
+        System.out.println("GAME OVER");
+    }
+}
